35880c49aa0b827f9c24b3695069e7427e7afe09
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-05-02 20:22:01 +0300

    Add files via upload

--- homework20.py
+++ homework20.py
@@ -1,8 +1,9 @@
 class House:
 
-    numberOfFloors = 10
+    def __init__(self):
+        self.numberOfFloors = 10
 
-    def function1(self, nuberOfFloors):
+    def function1(self, nuberOfFloors=None):
         for i in range(nuberOfFloors):
             print('Текущий этаж равен ', i + 1)
 
